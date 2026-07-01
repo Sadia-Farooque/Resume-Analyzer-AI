56f0219c89b8a57a4bf5e47c7d9335347f46240e
Sadia Fatima <anamfarooque06@gmail.com>
2026-07-01 20:54:05 +0500

    feat: build initial Resume Analyzer landing page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+function App(){
+  return(
+    <div>
+      <h1>Resume Analyzer AI</h1>
+      <p>Upload your resume and compare it with a job description using AI</p>
+      <button>Analyze Resume
+
+        
+      </button>
+    </div>
+  );
+}
+
+export default App;
